d74094ccde9afd08361c48687e5b3913065b329e
Slava Gapon <gapon.sv1026@gmail.com>
2023-11-01 14:42:23 +0300

    disable

---
+++ src/components/MusicInfo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="music-info">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'music-info', // Обновленное имя компонента
+}
+</script>
+
+<style scoped>
+.music-info {
+	padding: 10px;
+	background: rgba(0, 0, 0, 0.3);
+	width: 500px;
+  height: 75vh;
+	border-radius: 15px;
+}
+</style>
---
+++ src/components/MusicMenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="music-menu">
+		<h2>Menu</h2>
+		<p>Добро пожаловать в новое музыкальное приложение</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'music-menu', // Обновленное имя компонента
+}
+</script>
+
+<style scoped>
+.music-menu {
+	padding: 10px;
+	background: rgba(0, 0, 0, 0.3);
+	width: 300px;
+	height: 75vh;
+	border-radius: 15px;
+	color: aliceblue;
+	
+}
+
+</style>
